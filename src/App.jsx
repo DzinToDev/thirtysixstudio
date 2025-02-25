d6aacc96f9d84014909ca2196555bcceffbe6abc
DzinToDev <maheshdgaikwad18@gmail.com>
2025-02-26 04:10:51 +0530

    canvas animation done and first page dev done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import Canvas from "./components/Canvas";
+import data from "./data";
+import LocomotiveScroll from "locomotive-scroll";
+import { TfiLayoutMenuSeparated } from "react-icons/tfi";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  useEffect(() => {
+    const locomotiveScroll = new LocomotiveScroll();
+  }, []);
 
-export default App
+  return (
+    <>
+      <div className="w-full min-h-screen relative ">
+        {data[0].map((canvasdets, index) => (
+          <Canvas key={index} details={canvasdets}  />
+        ))}
+
+        <div className="w-full h-screen text-black ">
+          <nav className="w-full sm:py-3 sm:px-4 fixed top-0 left-0 z-50 flex justify-between font-['PPN-Reg'] border-b-[0.001px]">
+            <h1 className="text-base">Thirtysixstudio</h1>
+            <div className="links sm:flex gap-10 items-center ">
+              {[
+                "What we do",
+                "Who we are",
+                "How we give back",
+                "Talk to us",
+              ].map((elem, index) => {
+                return (
+                  <a
+                    key={index}
+                    href={`#${elem.toLowerCase()}`}
+                    className="text-sm hidden sm:block"
+                  >
+                    {elem}
+                  </a>
+                );
+              })}
+              <div>
+                <TfiLayoutMenuSeparated className="text-2xl ml-14" />
+              </div>
+            </div>
+          </nav>
+          <div className="center-text w-full h-2/3  flex items-center  justify-center sm:pl-16 py-12">
+            <div className=" sm:w-[53vw] h-full flex justify-between items-start relative py-12">
+            <div className="left-text sm:w-[45%] flex flex-col gap-8 ">
+              <h1 className="text-[2.3vw] leading-none ">At Thirtysixstudio, we build digital assets and immersive experiences for purposeful brands.</h1>
+              <p className="text-sm">We're a boutique production studio focused on design, animation, and technology, constantly rethinking what digital craft can do for present-day ads and campaigns.</p>
+              <span>Scroll</span>
+            </div>
+            <div className="h-full w-1/2  flex items-center justify-end">
+
+              <img className="w-2/3" src="./public/img/circleText.png" alt="" />
+            </div>
+            </div>
+          </div>
+          <div className="brand-heading px-4 mt-6 ">
+            <h1 className="text-[15.7vw] font-['PPN-Reg']">Thirtysixstudio</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Canvas.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useRef, useState } from "react";
+import canvasImages from "../canvasImage";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap/all";
+
+const Canvas = ({ details }) => {
+  const { startIndex, numImages, duration, size, top, left, zIndex } = details;
+  const [index, setIndex] = useState({ value: startIndex });
+  const canvasRef = useRef(null);
+
+  useGSAP(() => {
+    gsap.to(index, {
+      value: startIndex + numImages - 1,
+      duration: duration,
+      repeat: -1,
+      ease: "linear",
+      onUpdate: () => {
+        setIndex({ value: Math.round(index.value) });
+      },
+    });
+  });
+
+  useEffect(() => {
+    const scale = window.devicePixelRatio;
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+
+    const img = new Image();
+    img.src = canvasImages[index.value];
+
+    img.onload = () => {
+      canvas.width = canvas.offsetWidth * scale;
+      canvas.height = canvas.offsetHeight * scale;
+      canvas.style.width = canvas.offsetWidth + "px";
+      canvas.style.height = canvas.offsetHeight + "px";
+      ctx.scale(scale, scale);
+      ctx.drawImage(img, 0, 0, canvas.offsetWidth, canvas.offsetHeight);
+    };
+  }, [index]);
+  return (
+    <canvas
+      data-scroll
+      dats-scroll-speed={
+        // random value between 0 to 1
+        Math.random().toFixed(1)
+      }
+      ref={canvasRef}
+      className="absolute"
+      style={{
+        width: `${size * 1.3}px`,
+        height: `${size * 1.3}px`,
+        top: `${top}%`,
+        left: `${left}%`,
+        zIndex: `${zIndex}`,
+      }}
+      id="canvas"
+    ></canvas>
+  );
+};
+
+export default Canvas;
