e0d307267d08a22eff6e61428e7bdc7d71e83db7
DzinToDev <maheshdgaikwad18@gmail.com>
2025-03-01 18:13:10 +0530

    role section padding and text set

--- src/components/Awards.jsx
+++ src/components/Awards.jsx
@@ -14,9 +14,9 @@ const awardsData = [
 
 const Awards = () => {
   return (
-    <section className="flex flex-col items-center justify-center min-h-screen ">
+    <section className="flex flex-col items-center justify-center min-h-screen text-base ">
       <div className="w-4/5">
-      <p className="mb-4 px-4 py-2 text-xl font-semibold uppercase ml-49">
+      <p className="mb-4 px-4 py-2 text-base font-semibold uppercase ml-53">
         Role Open
       </p>
       </div>
@@ -24,17 +24,17 @@ const Awards = () => {
         {awardsData.map((award, index) => (
           <div
             key={index}
-            className="h-20 overflow-hidden cursor-pointer group"
+            className="h-17 bg-amber-200 overflow-hidden cursor-pointer group"
           >
-            <div className="relative h-40 transform -translate-y-20 transition-transform duration-500 ease-in-out group-hover:translate-y-0">
+            <div className="relative h-40 bg-red-200 transform -translate-y-20 transition-transform duration-500 ease-in-out group-hover:translate-y-0">
               {/* First Row */}
-              <div className=" bg-black text-[#fd2c2a] p-[1.67rem] flex items-center justify-center">
-                <h1 className="text-lg w-2/3">{award.name}</h1>
+              <div className=" bg-black text-[#fd2c2a] p-[1.77rem] flex items-center justify-center pl-12">
+                <h1 className=" w-2/3">{award.name}</h1>
                 {/* <h1 className="text-lg">{award.type}</h1> */}
               </div>
               {/* Second Row */}
-              <div className="bg-[#fd2c2a] text-black p-[1.67rem] flex items-center justify-center">
-                <h1 className="text-lg w-2/3">{award.project}</h1>
+              <div className="bg-[#fd2c2a] text-black p-[1.67rem] flex items-center justify-center pl-12">
+                <h1 className="w-2/3">{award.project}</h1>
                 {/* <h1 className="text-lg">{award.label}</h1> */}
               </div>
             </div>
